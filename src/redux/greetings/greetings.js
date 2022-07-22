5015ed73b6aad7b5f7405fc8500a38a192e22739
Ivan <ivankuartz@hotmail.com>
2022-07-22 14:01:08 -0300

    Add greetings page

---
+++ src/redux/greetings/greetings.js
@@ -0,0 +1,44 @@
+const initialState = [];
+
+const FETCH_GREETING = "FETCH_GREETING";
+const START_LOADING = "START_LOADING";
+const END_LOADING = "END_LOADING";
+
+export const getGreeting = () => async (dispatch) => {
+  dispatch({
+    type: START_LOADING,
+  });
+  const response = await fetch("http://127.0.0.1:3000/greeting");
+  const responseJSON = await response.json();
+  dispatch({
+    type: FETCH_GREETING,
+    data: responseJSON.greeting,
+  });
+  dispatch({
+    type: END_LOADING,
+  });
+};
+
+const greetingReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case START_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case END_LOADING:
+      return {
+        ...state,
+        loading: false,
+      };
+    case FETCH_GREETING:
+      return {
+        ...state,
+        message: action.data,
+      };
+    default:
+      return state;
+  }
+};
+
+export default greetingReducer;
