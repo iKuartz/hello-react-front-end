d13e5bbc7f6c083ea3dd86c9cf524510c4048f53
Ivan <ivankuartz@hotmail.com>
2022-07-22 14:02:02 -0300

    Configure redux

---
+++ src/redux/configStore.js
@@ -0,0 +1,13 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+
+import thunk from "redux-thunk";
+
+import greetingReducer from "./greetings/greetings";
+
+const rootReducer = combineReducers({
+  greetingReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
